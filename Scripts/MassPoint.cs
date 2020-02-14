75751c79aa94fa77a31bc02fb5e8149c1e8c4fea
adol919 <59782048+adol919@users.noreply.github.com>
2020-02-14 12:38:48 +0800

    xiugai f/2/14/12:38

---
+++ Scripts/MassPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MassPoint : MonoBehaviour
+{
+    public Transform tf;
+    void Start()
+    {
+        GetComponent<Rigidbody>().centerOfMass = tf.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
