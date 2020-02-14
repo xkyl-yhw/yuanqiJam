96501f1c6bf920bc85b0379d45223b80b1f61125
adol919 <fhb10@outlook.com>
2020-02-14 18:15:17 +0800

    fuhuabo6:15

--- Assets/Scripts/CarMove.cs
+++ Assets/Scripts/CarMove.cs
@@ -7,7 +7,7 @@ public class CarMove : MonoBehaviour
 
     public float carSpeed = 10;//基础速度
     float addSpeed = 0;//通过装备增加的前进速度
-    public float roateSpeed = 30;//转动速度
+    public float roateSpeed = 40;//转动速度
     float weight = 0;//负重
     float friction = 0.5f;//摩擦系数
     private static CarMove _Instance;
@@ -41,9 +41,16 @@ public class CarMove : MonoBehaviour
         v = Input.GetAxis("Vertical");
         if (v!=0)
         {
-            PoCheck();
+            if (v > 0)
+            {
+                FrontPoCheck();
+            }
+            else
+            {
+                BackPoCheck();
+            }
             float realspeed = RealSpeed();//计算真实速度
-            print("realspeed"+realspeed);
+            Debug.Log("realspeed"+realspeed);
             transform.Translate(Vector3.forward * Time.deltaTime*v*realspeed);
             if (h != 0)
             {
@@ -94,8 +101,9 @@ public class CarMove : MonoBehaviour
         return sp;
     }
 
-    void PoCheck()//检测坡
+    void FrontPoCheck()//前进时检测坡
     {
+       
         Vector3 nextPos = transform.position;
         Ray thisray = new Ray(nextPos, Vector3.down);//将车本身的图层设为ignore
         RaycastHit hitme;
@@ -127,11 +135,12 @@ public class CarMove : MonoBehaviour
 
         if(realPos.y>nextPos.y)//上坡
         {
+          
             float xiebian = tempPos.magnitude;
             float zhibian = Mathf.Abs(realPos.y - nextPos.y); //直角边
             if (xiebian > zhibian && xiebian > 0 && zhibian > 0)
             {
-                carSpeed = Mathf.Sqrt(xiebian * xiebian - zhibian * zhibian)*9;//速度 
+                carSpeed = Mathf.Sqrt(xiebian * xiebian - zhibian * zhibian)*10;//速度 
             }
             else
             {
@@ -140,6 +149,63 @@ public class CarMove : MonoBehaviour
         }
         else if(realPos.y == nextPos.y)//平地
         {
+
+            carSpeed = 10;
+        }
+        else//下坡
+        {
+  
+            carSpeed = 15;
+        }
+    }
+
+    void BackPoCheck()//后退时检测坡
+    {
+
+        Vector3 nextPos = transform.position;
+        Ray thisray = new Ray(nextPos, Vector3.down);//将车本身的图层设为ignore
+        RaycastHit hitme;
+        if (Physics.Raycast(thisray, out hitme))
+        {
+            nextPos = hitme.point;//自己的位置
+        }
+        else
+        {
+        }
+
+
+
+        Vector3 tempPos = transform.rotation * Vector3.forward * (10 + addSpeed - weight * friction) * 0.1f*-1;
+        Vector3 realPos = nextPos + tempPos + new Vector3(0, 10, 0);
+        Ray ray = new Ray(realPos, Vector3.down);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider == null)
+            {
+
+            }
+            else
+            {
+                realPos = hit.point;//得到下一步自己的位置  
+            }
+        }
+
+        if (realPos.y > nextPos.y)//上坡
+        {
+            float xiebian = tempPos.magnitude;
+            float zhibian = Mathf.Abs(realPos.y - nextPos.y); //直角边
+            if (xiebian > zhibian && xiebian > 0 && zhibian > 0)
+            {
+                carSpeed = Mathf.Sqrt(xiebian * xiebian - zhibian * zhibian) * 10;//速度 
+            }
+            else
+            {
+                carSpeed = 10;
+            }
+        }
+        else if (realPos.y == nextPos.y)//平地
+        {
             carSpeed = 10;
         }
         else//下坡
@@ -147,5 +213,4 @@ public class CarMove : MonoBehaviour
             carSpeed = 15;
         }
     }
-
 }
